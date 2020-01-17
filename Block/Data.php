1079792a9abbf318e18ffee48c1664770c7adb15
Paweł Złamaniec <pawel.zlamaniec@creativestyle.pl>
2020-01-17 08:31:23 +0100

    Added model view for the block

--- Block/Data.php
+++ Block/Data.php
@@ -4,29 +4,4 @@ namespace MageSuite\Pwa\Block;
 
 class Data extends \Magento\Framework\View\Element\Template
 {
-    /**
-     * @var \Magento\Store\Model\StoreManagerInterface
-     */
-    private $storeManager;
-
-    public function __construct(\Magento\Framework\View\Element\Template\Context $context, \Magento\Store\Model\StoreManagerInterface $storeManager, array $data = [])
-    {
-        $this->storeManager = $storeManager;
-        parent::__construct($context, $data);
-    }
-
-    public function getName()
-    {
-        return $this->storeManager->getStore()->getName();
-    }
-
-    public function getShortName()
-    {
-        return $this->storeManager->getStore()->getName();
-    }
-
-    public function getStartUrl()
-    {
-        return $this->storeManager->getStore()->getBaseUrl();
-    }
 }
