f113cc2faefebeaa907b35e241a65637172c541a
Paweł Złamaniec <pawel.zlamaniec@creativestyle.pl>
2020-01-21 10:20:46 +0100

    Fixed service worker test name

--- Test/Integration/Controller/ServiceWorker/IndexTest.php
+++ Test/Integration/Controller/ServiceWorker/IndexTest.php
@@ -4,7 +4,7 @@ namespace MageSuite\Pwa\Test\Integration\Controller\ServiceWorker;
 
 class IndexTest extends \Magento\TestFramework\TestCase\AbstractBackendController
 {
-    public function testOpenManifestUrl()
+    public function testOpenServiceWorkerUrl()
     {
         $this->dispatch("service-worker.js");
         $this->assertEquals(200, $this->getResponse()->getHttpResponseCode());
