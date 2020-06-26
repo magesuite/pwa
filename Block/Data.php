f481ec2be68628a153cf07f45f0ad01aefc16df0
Mateusz Krzeszowiak <mateusz.krzeszowiak@creativestyle.pl>
2020-06-26 09:38:22 +0200

    Fix translation file caching

--- Block/Data.php
+++ Block/Data.php
@@ -4,9 +4,4 @@ namespace MageSuite\Pwa\Block;
 
 class Data extends \Magento\Framework\View\Element\Template
 {
-    public function getMediaBaseUrl() {
-        return $this->_urlBuilder->getBaseUrl([
-            '_type' => \Magento\Framework\UrlInterface::URL_TYPE_MEDIA
-        ]);
-    }
 }
